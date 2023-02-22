980c8a1a6a32d83338f943ea1848c5299c4b1226
MIMBOL228 <aekemerov@yandex.ru>
2023-02-22 19:31:11 +0500

    bug3

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var routing_controllers_1 = require("routing-controllers");
-var UserController_1 = require("./Controllers/UserController");
-var app = (0, routing_controllers_1.createExpressServer)({
-    controllers: [UserController_1.UserController]
-});
-app.listen(3000);

1a358824bed46ec72a8361b4ee16cb7d303200fb
MIMBOL228 <aekemerov@yandex.ru>
2023-02-23 02:52:51 +0500

    Maybe all work

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
+import 'reflect-metadata';
 import { createExpressServer } from 'routing-controllers';
-import {UserController} from './Controllers/UserController';
-const app = createExpressServer({
-  controllers: [UserController], 
+import path from 'path';
+import { dirname } from 'node:path';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+createExpressServer({
+  controllers: [path.join(dirname + '/controllers/*.js')],
+}).listen(process.env.PORT, ()=>{
+  console.log("Server started on port " + process.env.PORT)
 });
-app.listen(3000);
---
+++ tests/index.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { AsyncAdapter, NodeProvider } from '@stenodb/node'
+import { Post, User, Users } from './entities.js'
+
+const path = resolve(dirname(fileURLToPath(import.meta.url)), '..', 'db')
+
+const initialData = new Users(new User('John Doe'))
+const adapter = new AsyncAdapter('users', Users, initialData)
+const provider = new NodeProvider({ path })
+
+const database = await provider.create(adapter)
+await database.read()
+const post = new Post('Hello world')
+database.data?.users[0]?.addPost(post)
+await database.write()
+
+console.log(database.data?.users[0].posts)

fcc0651846bfc4cb5de9ca0342c1e0ddfad0da03
MIMBOL228 <aekemerov@yandex.ru>
2023-02-22 19:18:41 +0500

    bugs2

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { createExpressServer } from 'routing-controllers';
+import { UserController } from './Controllers/UserController';
+const app = createExpressServer({
+  controllers: [UserController], 
+});
+app.listen(3000);

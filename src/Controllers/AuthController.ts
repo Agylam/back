d6b57799b2563a28efb122feb9be0aa0ba4a6e78
MIMBOL228 <aekemerov@yandex.ru>
2023-03-01 23:17:57 +0500

    Doesnt work

--- src/Controllers/AuthController.ts
+++ src/Controllers/AuthController.ts
@@ -1,14 +1,28 @@
-import { JsonController, Param, Body, Get, Post, Put, Delete } from 'routing-controllers';
+import { JsonController, Post, Body } from 'routing-controllers';
+import jwt from 'jsonwebtoken';
 
-@JsonController()
+interface LoginBody {
+  email: string;
+  password: string;
+}
+
+@JsonController('/auth')
 export class AuthController {
-  @Get('/users')
-  getAll() {
-    return {"a":"a","b":"b"};
-  }
+  private readonly JWT_SECRET: string = 'supersecretkey';
 
-  @Get('/users/:id')
-  getOne(@Param('id') id: number) {
-    return {"a":"a","b":id};
+  @Post('/login')
+  async login(@Body() body: LoginBody) {
+    // Here you would check the user credentials against a database or other source
+    // For demonstration purposes, we will just create a fake user with a password of "password"
+    const user = { email: body.email };
+    if (body.password !== 'password') {
+      throw new Error('Incorrect password');
+    }
+
+    // If the user credentials are valid, generate a JWT token
+    const token = jwt.sign({ email: user.email }, this.JWT_SECRET, { expiresIn: '1h' });
+
+    // Return the token to the client
+    return { token };
   }
 }
